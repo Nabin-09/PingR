5d8ef9e06f2e4e1827cb2b838b828e01148c7301
Nabin <sharmanabin.0910@gmail.com>
2025-08-23 14:16:13 +0530

    Setting up divisions in files

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,12 @@
 import express from "express";
+import { ENV } from "./config/env.js";
+
 const app = express();
 
-
+const PORT = process.env.PORT;
 app.get("/" , (req , res)=>{
      res.send("Hello World")
 })
-app.listen(5001 , ()=>{
-    console.log("Server running on port 5001");
+app.listen(ENV.PORT , ()=>{
+    console.log("Server running on port:", ENV.PORT);
 })
