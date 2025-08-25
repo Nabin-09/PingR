86034a6f4069f7a016829d1c641d77ef243be242
Nabin <sharmanabin.0910@gmail.com>
2025-08-25 20:42:46 +0530

    Added Home page and Auth Page'

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { ClerkProvider } from '@clerk/clerk-react'
+import {BrowserRouter} from "react-router-dom"
 import App from './App.jsx'
 
 // Import your Publishable Key
@@ -14,7 +15,9 @@ if (!PUBLISHABLE_KEY) {
 createRoot(document.getElementById('root')).render(
   <StrictMode>
      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-        <App />
+        <BrowserRouter>
+          <App/>
+        </BrowserRouter>
       </ClerkProvider>
   </StrictMode>,
 )
